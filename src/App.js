c5141cc9b279f9e74abe1c2663026551c15134c3
GuimelAlencar <guimel.alencar.s@gmail.com>
2024-02-07 17:11:45 -0400

    feature addClass

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import './App.css';
 import { useRef, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+
+  
   const inputRef = useRef(null);
+  
+
+  
+  const [tasks, setTasks] = useState([])
+  function addTask() {
+    const newTask = {
+      id: uuidv4(),
+      description: inputRef.current.value
+    }
+
+  const newTasks = [newTask, ...tasks]
+  setTasks(newTasks)
+  }
+
 
   return (
     <div classname="App">
-      <cd />
       <input ref={inputRef} name='toDoInput'/>
-      <button>Add</button>
-
+      <button onClick={addTask} >Add</button>
+      {tasks.map((task, index) => (
+        <div key={index}>
+          <span>{task.description}</span>
+          <br />
+        </div>
+      ))}
     </div>
   );
 }
