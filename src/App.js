1fcfe2a9d937097283d6c0c13086f4cc104e600e
GuimelAlencar <guimel.alencar.s@gmail.com>
2024-02-07 08:03:13 -0400

    Basic Structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+  const inputRef = useRef(null);
+
   return (
+    <div classname="App">
+      <cd />
+      <input ref={inputRef} name='toDoInput'/>
+      <button>Add</button>
+
+    </div>
   );
 }
 
