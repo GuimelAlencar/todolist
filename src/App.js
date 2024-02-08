5a351546e827fa7529af444d82c2a521e5e539b5
GuimelAlencar <guimel.alencar.s@gmail.com>
2024-02-08 17:12:06 -0400

    comments

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import { useRef, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  // Imput
   const inputRef = useRef(null);
   
+  //Create Tasks
   const [tasksList, setTasksList] = useState([])
   function addTask() {
     const newTask = {
       id: uuidv4(),
       description: inputRef.current.value
     }
-
   const newTasksList = [newTask, ...tasksList];
   setTasksList(newTasksList);
   }
 
+  //Delete Tasks
   function deleteTask(taskToBeDeleted){
     const newTasksList = tasksList.filter(task => task !== taskToBeDeleted);
     setTasksList(newTasksList);
